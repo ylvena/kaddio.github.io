9b02e5c9f26a8b2837fb00d34cabc26f566cc389
David Jørgensen <david@pnd.se>
2015-10-20 17:33:30 +0200

    directives

--- js/front.js
+++ js/front.js
@@ -1,6 +1,42 @@
 var ePsyFront = angular.module('ePsyFront', [
 	'ngRoute',
     'lbServices'
-])
+]);
 
 env(ePsyFront);
+
+
+ePsyFront.directive('emailLink', [function() {
+	return {
+		restrict: 'A',
+		link: function($scope, $element, $attr){
+			$element.bind('click', function(event){
+				ga('send', 'event', 'link', 'click', 'email');
+			});
+		}
+	};
+}])
+
+
+.directive('formLink', [function() {
+	return {
+		restrict: 'A',
+		link: function($scope, $element, $attr){
+			$element.bind('click', function(event){
+				ga('send', 'event', 'link', 'click', 'google-form');
+			});
+		}
+	};
+}])
+
+.directive('newsletter', [function() {
+	return {
+		restrict: 'A',
+		templateUrl: 'partials/newsletter.html',
+		link: function($scope, $element, $attr){
+			$scope.signup = function(){
+
+			};
+		}
+	};
+}]);
